fca8f3819eb0214e684e1ad943c64ca403dd8925
Bolot Azatov <bolotazatov23.05.03@gmail.com>
2026-09-16 18:15:26 +0600

    Мелкие исправления

--- src/main/java/com/example/instajava/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/instajava/service/impl/PostServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.instajava.service.impl;
 
+import com.example.instajava.dto.response.PostResponseDto;
 import com.example.instajava.exception.ResourceNotFoundException;
+import com.example.instajava.models.Post;
 import com.example.instajava.repository.PostRepository;
 import com.example.instajava.service.PostService;
 import com.example.instajava.service.UserService;
@@ -8,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -24,5 +29,35 @@ public class PostServiceImpl implements PostService {
             throw new ResourceNotFoundException("Пользователь с id " + userId + " не найден");
         }
         return postRepository.countByAuthorId(userId);
+    }
+
+    @Override
+    public PostResponseDto createPost(MultipartFile image, String caption, Long authorId) {
+        return null;
+    }
+
+    @Override
+    public void deletePost(Long postId, Long currentUserId) {
+
+    }
+
+    @Override
+    public List<PostResponseDto> getFeed(Long currentUserId) {
+        return List.of();
+    }
+
+    @Override
+    public PostResponseDto getPostById(Long postId) {
+        return null;
+    }
+
+    @Override
+    public Post getPostEntityById(Long postId) {
+        return null;
+    }
+
+    @Override
+    public List<PostResponseDto> getUserPosts(Long userId) {
+        return List.of();
     }
 }
