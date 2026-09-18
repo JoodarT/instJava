0d9063c9dcd0a84a83f619647a7aff109506f477
Joodar <joodartynystanov@gmail.com>
2026-09-18 15:40:42 +0600

    удалил дублирующий UserAlreadyExistException

--- src/main/java/com/example/instajava/exception/UserAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.instajava.exception;
-
-public class UserAlreadyExistException extends RuntimeException {
-    public UserAlreadyExistException(String message) {
-        super(message);
-    }
-}
